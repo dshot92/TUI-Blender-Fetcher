9f4bc77c02b8302f5dee161478d0647e1cd81249
dshot92 <dshot92@gmail.com>
2025-04-03 13:23:53 +0200

    cleaner layout

--- tui/views.go
+++ tui/views.go
@@ -5,25 +5,22 @@ import (
 )
 
 func (m *Model) renderPageForView() string {
-	header := m.renderCommonHeader()
-	headerHeight := 5
+
+	header := headerStyle.Width(m.terminalWidth).AlignHorizontal(lp.Center).Render("TUI Blender Launcher")
+	headerHeight := 6
+
 	var footer string
-	var footerHeight int
+	var footerHeight int = 1
 	var content string
+
 	if m.currentView == viewInitialSetup || m.currentView == viewSettings {
 		footer = m.renderSettingsFooter()
-		footerHeight = 2
 		content = m.renderSettingsContent(m.terminalHeight - headerHeight - footerHeight)
 	} else {
 		footer = m.renderBuildFooter()
-		footerHeight = 1
 		content = m.renderBuildContent(m.terminalHeight - headerHeight - footerHeight)
 	}
+
 	baseView := lp.JoinVertical(lp.Top, header, content, footer)
 	return lp.Place(m.terminalWidth, m.terminalHeight, lp.Left, lp.Top, baseView)
-}
-
-// renderCommonHeader returns the common header (title) for both builds and settings pages.
-func (m *Model) renderCommonHeader() string {
-	return headerStyle.Width(m.terminalWidth).AlignHorizontal(lp.Center).Render("TUI Blender Launcher")
 }
